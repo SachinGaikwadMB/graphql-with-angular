abdc93d1805d46d19da29fc20f2cafc76eb26fb1
= <=>
2023-03-08 12:25:58 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'graphql-demo-ui';
+  books : any = [];
+
+  constructor(private apollo : Apollo) {}
+
+ 
+  ngOnInit(): void {
+    this.apollo.watchQuery({
+      query : gql`
+      query  {
+        getBooks {
+            title
+        }
+    }
+    `
+    }).valueChanges.subscribe((resp) => {
+      this.books = resp.data;
+
+      console.log(this.books);
+      console.log(this.books['getBooks'][0].title);
+      
+    });
+  }
 }
